4a7724e295b3a4df38229cf3139d9e835b9991b8
Subhajeet Lahiri <subhajeetlahiri@gmail.com>
2022-09-29 18:34:08 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          <div className="col"></div>
+          <div className="col"><h1>Hello! This is Subhajeet</h1></div>
+          <div className="col"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
