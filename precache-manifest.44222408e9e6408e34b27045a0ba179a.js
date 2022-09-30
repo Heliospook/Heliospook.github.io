c5381324c3b28d0e2c8028ce17eb2926414472ef
Subhajeet Lahiri <subhajeetlahiri@gmail.com>
2022-10-01 00:22:40 +0530

    Updates

---
+++ precache-manifest.44222408e9e6408e34b27045a0ba179a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "469d6c15ff218dea4278",
+    "url": "/static/css/main.b95aa311.chunk.css"
+  },
+  {
+    "revision": "469d6c15ff218dea4278",
+    "url": "/static/js/main.469d6c15.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "7d89464741f2193926da",
+    "url": "/static/js/2.7d894647.chunk.js"
+  },
+  {
+    "revision": "dc951fff484053e1c299",
+    "url": "/static/js/runtime~main.dc951fff.js"
+  },
+  {
+    "revision": "f4e4f09d09fc1a3d8ff9c644a4d3f8ab",
+    "url": "/index.html"
+  }
+];
